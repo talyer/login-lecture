79230bfa646d603301c3f1955b6691ea2c699b65
현준 <92567978+talyer@users.noreply.github.com>
2024-11-22 21:26:43 +0900

    Login Test 로그인 내용 저장하는 시스템 추가

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.hello);
+router.get("/", ctrl.output.home);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
 
-router.get("/login", ctrl.login);
     
 module.exports = router;
