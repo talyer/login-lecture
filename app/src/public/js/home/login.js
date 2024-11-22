7fa7403ee7e05b315ad5ca1f971ab63e2f561516
현준 <92567978+talyer@users.noreply.github.com>
2024-11-22 20:57:00 +0900

    Login Test +fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -10,8 +10,14 @@ const id = document.querySelector("#id"),
         const req = { 
             id : id.value,
             psword: psword.value,
-        
         };
-    console.log(req);
+
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req)
+        });
     
-    }
+    }   
