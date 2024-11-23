0212419293f6e0a2fe96c0e6ff0efed8fc9fc2cf
현준 <92567978+talyer@users.noreply.github.com>
2024-11-23 14:52:21 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,22 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    comfirmPsword = document.querySelector("#comfirm-psword"),
+    registerBtn = document.querySelector("#button");
 
-    loginBtn.addEventListener("click", login);
+    registerBtn.addEventListener("click", register);
 
-    function login() {
+    function register() {
         const req = { 
             id : id.value,
+            name: name.value,
             psword: psword.value,
+            comfirmPsword: comfirmPsword.value,
         };
 
-        fetch("/login", {
+        fetch("/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -22,13 +26,13 @@ const id = document.querySelector("#id"),
         .then((res) => res.json())
         .then((res) => {
             if (res.success) {
-                location.href = "/"; // 로그인 성공시 원래 사이트로 돌아가기
+                location.href = "/login"; // 로그인 성공시 원래 사이트로 돌아가기
             } else {
                 alert(res.msg);
             }
         })
         .catch((err) => { // 인터넷이 안되거나 사이트 문제가 있을시 나오는 문구
-            console.error(new Error("Login error"));
+            console.error(new Error("register error"));
         });
     
     }   
